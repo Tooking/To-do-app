feca5a4f77addb43905b106967bbe8d44abf9440
unknown <andreytseshko@gmail.com>
2022-12-26 13:19:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const path = require('path')
+const {v4} = require('uuid')
+const app = express()
+
+let TASKS = [
+    {id: v4(), name: "Buy a bread", value: "tomorrow", marked: false}
+]
+
+app.use(express.json())
+
+//GET
+app.get('/api/tasks', (req, res)=>{
+        res.status(200).json(TASKS)
+})
+
+
+//POST
+app.post('/api/tasks', (req, res) =>{
+    const task = {...req.body, id: v4(), marked: false}
+    TASKS.push(task)
+    res.status(201).json(task)
+})
+
+//DELETE
+app.delete('/api/tasks/:id', (req, res) =>{
+    TASKS = TASKS.filter(t => t.id !== req.params.id)
+    res.status(200).json({message: "Task has been deleted"})
+})
+
+//PUT
+app.put('/api/tasks/:id', (req, res)=>{
+    const idx = TASKS.findIndex(t => t.id === req.params.id)
+    TASKS[idx] = req.body
+    res.json(TASKS[idx])
+})
+
+
+app.use(express.static(path.resolve(__dirname, 'client')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "index.html"))
+})
+
+app.listen(3000, ()=> console.log("Server has been started on port 3000..."))
---
+++ client/frontend.js
@@ -0,0 +1,81 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+
+Vue.component('loader', {
+    template: `
+            <div style="display: flex; justify-content: center; align-items: center;">
+                <div class="spinner-border" role="status">
+                    <span class="visually-hidden"></span>
+                </div>
+            </div>
+                `
+}) 
+
+new Vue({
+    el: '#app',
+    data(){
+        return {
+            loading: false,
+            form: {
+                name: '',
+                value: ''
+            },
+        tasks: []
+        }
+    },
+    computed:{
+        canCreate(){
+            return this.form.value.trim() && this.form.name.trim()
+        }
+    },
+    methods: {
+        async createTask() {
+            const {...task} = this.form
+
+            const newTask = await request('/api/tasks', 'POST', task)
+
+            this.tasks.push(newTask)
+
+            this.form.name = this.form.value = '';
+        },
+        async markTask(id){
+            const task = this.tasks.find(t => t.id === id)
+            const updated = await request(`/api/tasks/:${id}`, 'PUT', {
+                ...task, 
+                marked: true
+            })
+            task.marked = updated.marked;
+            
+        },
+        async removeTask(id){
+            await request(`/api/tasks/${id}`, 'DELETE')
+            this.tasks = this.tasks.filter(t => t.id !== id)
+        }
+    },
+    async mounted(){
+        this.loading = true;
+        this.tasks = await request('/api/tasks');
+        this.loading = false;
+    }
+})
+
+
+async function request(url, method = "GET", data = null){
+    try{
+        const headers = {}
+        let body
+
+        if(data){
+            headers['Content-Type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return await response.json()
+    }catch(e){
+        console.warn("Error:", e.message)
+    }
+}
